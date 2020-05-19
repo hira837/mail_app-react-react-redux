6c49758354e3056a86bee178eaab6a9ae5de448c
Yuki Hirayama <gfyxm868@gmail.com>
2020-05-20 01:42:03 +0900

    test props

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+// Class Component
+// class App extends Component {
+//   render() {
+//     return (
+//       <React.Fragment>
+//         <label htmlFor="bar">bar</label>
+//         <input type="text" onChange={() => { console.log("hi") }} />
+//       </React.Fragment>
+//     )
+//   }
+// }
+
+// Functional Component
+const App = () => {
+  const profiles = [
+    { name: "Taro", age: 10 },
+    { name: "Hanako", age: 5 },
+    { name: "Yuki" }
+  ]
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {
+        profiles.map((profile, index) => {
+          return <User name={profile.name} age={profile.age} key={index} />
+        })
+      }
     </div>
-  );
+  )
+}
+const User = (props) => {
+return <div>Hi I am {props.name} and {props.age} yeas old.</div>
+}
+
+User.defaultProps = {
+  age: 1
 }
 
 export default App;
