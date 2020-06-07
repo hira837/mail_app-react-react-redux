c287a43456f69e362794481fb3c38898db11fcd2
Yuki Hirayama <gfyxm868@gmail.com>
2020-06-07 21:19:53 +0900

    modify filtering

--- src/reducers/mails.js
+++ src/reducers/mails.js
@@ -15,10 +15,21 @@ export default (mails = {}, action) => {
       delete mails[action.id];
       return { ...mails };
     case SORT_BY_ASC:
-      let dataForAsc = action.response.filter((item) => action.validDateRange.includes(item.date))
+      let dataForAsc
+      if (!action.validDateRange.length) {
+        dataForAsc = action.response
+      } else {
+        dataForAsc = action.response.filter((item) => action.validDateRange.includes(item.date))
+      }
+  
       return _.orderBy(dataForAsc, 'date', 'asc')
     case SORT_BY_DESC:
-      let dataForDesc = action.response.filter((item) => action.validDateRange.includes(item.date))
+      let dataForDesc
+      if (!action.validDateRange.length) {
+        dataForDesc = action.response
+      } else {
+        dataForDesc = action.response.filter((item) => action.validDateRange.includes(item.date))
+      }
       return _.orderBy(dataForDesc, "date", "desc");
     case FILTER_BY_DATE:
       const filteredData = action.response.filter((item) => action.validDateRange.includes(item.date))
